176c1aa7bae63143351f2dc919fd59f0ddf92ce4
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-05-14 12:11:51 +0530

    deleted:    src/app/(home)/products/page.tsx 	modified:   src/app/checkout/page.tsx

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,8 @@
 import { CheckoutOne } from "@/components/commercn/checkouts/checkout-01";
 
-export default function CheckoutPage() {
+export const dynamic = "force-dynamic";
 
-  
+export default function CheckoutPage() {
 
   return (
     <div className="h-full flex justify-center items-start py-6">
